a39245918b39e3fa1e89cf4b3327af3805031286
hjmc0 <hjmc0@outlook.com>
2023-02-25 12:26:22 +0800

    feature/updated display table

--- frontend/src/components/Claims.js
+++ frontend/src/components/Claims.js
@@ -1,18 +1,87 @@
 import React from 'react';
-import ClaimItem from './ClaimItem';
-const Claims = (props) => {
-    return (
-        <div class="table-responsive">
-            <table class='table table-bordered'>
-                {
-                    props.claims.map(claim => (
-                        <ClaimItem key={claim.ClaimID} claim={claim} />
-                    ))
-                }
-            </table>
-        </div>
-        
-    )
-}
+import { Table, Button } from 'antd'
 
+const columns = [
+    {
+        title: 'Claim ID',
+        dataIndex: 'ClaimID',
+        key: 'ClaimID',
+    },
+    {
+        title: 'InsuranceID',
+        dataIndex: 'InsuranceID',
+        key: 'InsuranceID',
+    },
+    {
+        title: 'FirstName',
+        dataIndex: 'FirstName',
+        key: 'FirstName',
+    },
+    {
+        title: 'ExpenseDate',
+        dataIndex: 'ExpenseDate',
+        key: 'ExpenseDate',
+    },
+    {
+        title: 'Amount',
+        dataIndex: 'Amount',
+        key: 'Amount',
+    },
+    {
+        title: 'Purpose',
+        dataIndex: 'Purpose',
+        key: 'Purpose',
+    },
+    {
+        title: 'FollowUp',
+        dataIndex: 'FollowUp',
+        key: 'FollowUp',
+    },
+    {
+        title: 'PreviousClaimID',
+        dataIndex: 'PreviousClaimID',
+        key: 'PreviousClaimID',
+    },
+    {
+        title: 'Status',
+        dataIndex: 'Status',
+        key: 'Status',
+    },
+    {
+        title: 'LastEditedClaimDate',
+        dataIndex: 'LastEditedClaimDate',
+        key: 'LastEditedClaimDate',
+    },
+    {
+        title: 'Actions',
+        key: 'actions',
+        render: (text, record) => (
+            <>
+                <Button type="primary">
+                    Edit
+                </Button>
+                <Button type="danger">
+                    Delete
+                </Button>
+            </>
+        ),
+    },
+];
+
+
+const Claims = (props) => {
+    /** const handleEdit = (record) => {
+      // handle edit logic
+      console.log('Edit claim:', record);
+    };
+  
+    const handleDelete = (record) => {
+      // handle delete logic
+      console.log('Delete claim:', record);
+    };*/
+  
+    return (
+      <Table columns={columns} dataSource={props.claims} rowKey="ClaimID" />
+    );
+  };
 export default Claims
