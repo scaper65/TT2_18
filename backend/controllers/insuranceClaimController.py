9748a0e79e8cc3874d11954e2fe3901cfce856a4
Eeki <scaper65@hotmail.com>
2023-02-25 12:10:15 +0800

    Add: initial commit for edit claim

--- backend/controllers/insuranceClaimController.py
+++ backend/controllers/insuranceClaimController.py
@@ -5,6 +5,42 @@ from utils.dbConfig import db
 
 @jwt_required()
 
+def editClaim(claimId):
+    
+    try:
+        FirstName = request.json.get("FirstName", None)
+        LastName = request.json.get("LastName", None)
+
+        ExpenseDate = request.json.get("ExpenseDate", None)
+        Amount = request.json.get("Amount", None)
+
+        
+        Purpose = request.json.get("Purpose", None)
+        FollowUp = request.json.get("FollowUp", None)
+
+        PreviousClaimID = request.json.get("Purpose", None)
+       
+        claim = InsuranceClaim(FirstName=FirstName,LastName=LastName,ExpenseDate=ExpenseDate,Amount=Amount,FirstName=FirstName,Purpose=Purpose,FollowUp=FollowUp,PreviousClaimID=PreviousClaimID)
+        
+        InsuranceClaim.query.filter(InsuranceClaim.ClaimID == claimId).update()
+
+        db.session.add(claim)
+        db.session.commit()
+
+        return jsonify({
+            "code": 200,
+            "message": "Successfully Added a Claim!"
+        })
+
+    except Exception as e:
+        print(e)
+        return jsonify(
+            {
+                "code": 500,
+                "message": "Server Error"
+            }
+        ), 500
+
 
 def addClaim():
     try:
