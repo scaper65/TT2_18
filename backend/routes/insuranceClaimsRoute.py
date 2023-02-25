50d83a42488f3b070a78f897829de4ecefdaf4b7
Eeki <scaper65@hotmail.com>
2023-02-25 12:15:24 +0800

    Change: added claims route

---
+++ backend/routes/insuranceClaimsRoute.py
@@ -0,0 +1,19 @@
+from flask import Blueprint
+
+from controllers.insuranceClaimsController import *; 
+
+insuranceClaimsRoute = Blueprint('insuranceClaim', __name__); 
+
+
+insuranceClaimsRoute.route('/getall', strict_slashes=False,
+                methods=['GET'])(getall)
+
+insuranceClaimsRoute.route('/delete', strict_slashes=False,
+                methods=['POST'])()
+
+insuranceClaimsRoute.route('/edit', strict_slashes=False,
+                methods=['POST'])(getall)
+
+
+insuranceClaimsRoute.route('/add', strict_slashes=False,
+                methods=['POST'])(getall)
